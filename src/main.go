cad5e69b4312680b25ce2bf716db7020999178aa
tehdarthvid <darthvid@hotmail.com>
2020-01-30 03:08:57 +0800

    figuring out what I want

--- src/main.go
+++ src/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha512"
+	"fmt"
+	"time"
+)
+
+// An Encounter and relevant details
+type Encounter struct {
+	Arena     string
+	Health    uint8
+	Monesters []byte
+	Items     []byte
+	Encounter string
+	Checksum  []byte
+}
+
+func getTime() int64 {
+	return time.Now().UnixNano()
+}
+
+func getWhereAmI() string {
+	return "dev"
+}
+
+func getMonster(seed int64) string {
+	return "k"
+}
+
+func generateEncounter() string {
+	return "<.\\" + ".@!.[..." + getMonster(getTime()) + "..\u2640|"
+}
 
 func main() {
-	fmt.Println("Hello, 世界")
+	hasher := sha512.New()
+	hasher.Write([]byte("string"))
+
+	//sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	fmt.Println("世界[" + getWhereAmI() + "] = " + generateEncounter())
 }
