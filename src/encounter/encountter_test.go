da9d99508589796fec90e3101b9f70d78f5462d4
tehdarthvid <darthvid@hotmail.com>
2020-02-05 23:40:42 +0800

    added env log

--- src/encounter/encountter_test.go
+++ src/encounter/encountter_test.go
@@ -2,22 +2,18 @@ package encounter
 
 import "testing"
 
-func TestGenerateEncounter(t *testing.T) {
-	res := GenerateEncounter()
+func TestCreate(t *testing.T) {
+	res := Create()
 	if (0 >= len(res.Floor)) && (16 < len(res.Floor)) {
 		t.Errorf("len(generateEncounter) = %d; want: 1..16", len(res.Floor))
-	} else {
-		//t.Logf("len(generateFloor()) = %d; want <= 16", len(res))
 	}
-
 }
 
-func TestDeploymentEnv(t *testing.T) {
+func TestGetWhereAmI(t *testing.T) {
 	res := getWhereAmI()
 	if "" == res {
-		t.Errorf("OLD_DEPLOYMENT_ENV = <empty>; want <not empty>")
+		t.Errorf("OLD_TARGET_ENV = <empty>; want <not empty>")
 	} else {
-		//t.Logf("len(generateFloor()) = %d; want <= 16", len(res))
+		t.Log("OLD_TARGET_ENV = " + res)
 	}
-
 }
