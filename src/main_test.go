a54e7261a03aac1b234d35398d92aedd1aea8af1
tehdarthvid <darthvid@hotmail.com>
2020-02-06 16:39:13 +0800

    unit test for env secret

--- src/main_test.go
+++ src/main_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetSecret(t *testing.T) {
+	res := os.Getenv("OLD_SECRET")
+	if "" == res {
+		t.Errorf("OLD_SECRET = <empty>; want <not empty>")
+	} else {
+		t.Log("OLD_SECRET = " + res)
+	}
+}
